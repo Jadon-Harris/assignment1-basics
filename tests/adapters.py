26f81d94a7006325debba5d27d9f3cf4877ed6fb
Jadon-Harris <jadonharris331@gmail.com>
2026-05-26 20:38:14 +0800

    Update adapters.py

--- tests/adapters.py
+++ tests/adapters.py
@@ -11,6 +11,8 @@ from jaxtyping import Bool, Float, Int
 from torch import Tensor
 
 from cs336_basics.bpe_trainer import train_bpe_tokenizer
+from cs336_basics.module.rope import RoPE
+from cs336_basics.module.softmax import softmax
 
 
 def run_linear(
@@ -435,7 +437,7 @@ def run_softmax(in_features: Float[Tensor, " ..."], dim: int) -> Float[Tensor, "
         Float[Tensor, "..."]: Tensor of with the same shape as `in_features` with the output of
         softmax normalizing the specified `dim`.
     """
-    raise NotImplementedError
+    return softmax(in_features, dim)
 
 
 def run_cross_entropy(
